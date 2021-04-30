1c1fec7947e2f35e2b1b8276efcd833b6f2f83ce
Uendy <alex.beshkov98@gmail.com>
2021-04-30 20:54:24 +0300

    Adding: -Telerik Courses Main folder: --Module 0 folder: ---Practical Tasks 1 folder: ----Q01 Game.cs

---
+++ Telerik-Courses-main/Telerik-Courses-main/Module 0/Practical Tasks 1/Q01 Game/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        var input = Console.ReadLine()
+            .ToCharArray()
+            .Select(x => 
+            int.Parse(x.ToString()))
+            .OrderByDescending(x => x)
+            .ToArray();
+
+        int biggestSum = input[0] * input[1];
+
+        Console.WriteLine(Math.Max(biggestSum * input[2], biggestSum + input[2]));
+    }
+}
