4d25e9e8adeaa6b3bb0d03b287c0f8ebd96351a3
Uendy <alex.beshkov98@gmail.com>
2021-05-01 20:28:09 +0300

    Adding: -Q02 Electronic Message

---
+++ Telerik-Courses-main/Telerik-Courses-main/Module 0/Practical Tasks 1/Q02 Electronic Message/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine().ToCharArray();
+
+        var maxNonDesiredSymbols = 0;
+        var currentNonDesiredSymbols = 0;
+
+        for (int i = 0; i < input.Count(); i++)
+        {
+            var currentChar = input[i];
+
+            if (char.IsLetterOrDigit(currentChar) || currentChar == ' ')
+            {
+                if (currentNonDesiredSymbols > maxNonDesiredSymbols)
+                {
+                    maxNonDesiredSymbols = currentNonDesiredSymbols;
+                }
+                currentNonDesiredSymbols = 0;
+            }
+            else 
+            {
+                currentNonDesiredSymbols++;   
+            }
+        }
+
+        Console.WriteLine(Math.Max(maxNonDesiredSymbols, currentNonDesiredSymbols));
+    }
+}
---
+++ Telerik-Courses-main/Telerik-Courses-main/README.md
@@ -0,0 +1,2 @@
+# Telerik-Courses
+My work from the courses from Telerik Alpha Academy
