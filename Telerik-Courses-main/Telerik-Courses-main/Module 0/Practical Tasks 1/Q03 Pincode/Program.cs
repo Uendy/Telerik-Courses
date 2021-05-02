89d38e4321b8a312f9443c0e5f22f901ca82f864
Uendy <alex.beshkov98@gmail.com>
2021-05-02 18:41:21 +0300

    Adding: -Q03 Pincode (Finished)

---
+++ Telerik-Courses-main/Telerik-Courses-main/Module 0/Practical Tasks 1/Q03 Pincode/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Recieve two numbers, reverse them and print the bigger
+        // Example: Input: 234 145 Output: 541
+
+        var numbers = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+        var firstNumber = int.Parse(new string(numbers[0].Reverse().ToArray()));
+        var secondNumber = int.Parse(new string(numbers[1].Reverse().ToArray()));
+
+        Console.WriteLine(Math.Max(firstNumber, secondNumber));
+    }
+}
