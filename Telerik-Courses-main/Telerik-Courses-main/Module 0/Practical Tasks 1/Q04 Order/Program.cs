8091735d4e16514eb90de23146dca3390607f0ce
Uendy <alex.beshkov98@gmail.com>
2021-05-03 11:48:11 +0300

    Adding: -Q04 Order (start) // Tried to be smart... but it didnt work

---
+++ Telerik-Courses-main/Telerik-Courses-main/Module 0/Practical Tasks 1/Q04 Order/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Recieve four values and determine their order: ascending, descending, mixed
+        // Example: Input: 3, 4, 4, 5 Output: mixed
+
+        var array = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+        if (array == array.OrderByDescending(x => x).ToArray())
+        {
+            Console.WriteLine("Descending");
+        }
+        else if (array == array.OrderBy(x => x).ToArray())
+        {
+            Console.WriteLine("Ascending");
+        }
+        else
+        {
+            Console.WriteLine("Mixed");
+        }
+    }
+}
