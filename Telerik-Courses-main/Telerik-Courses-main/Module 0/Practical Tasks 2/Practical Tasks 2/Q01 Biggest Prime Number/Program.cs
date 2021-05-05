42389b401b714fac73a27667e604e97a0e856196
Uendy <alex.beshkov98@gmail.com>
2021-05-05 19:42:15 +0300

    Adding: -Practical Tasks 2 folder: --Q01 Biggest Prime Number

---
+++ Telerik-Courses-main/Telerik-Courses-main/Module 0/Practical Tasks 2/Practical Tasks 2/Q01 Biggest Prime Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Find the biggest Prime Number between 1 and N and print it
+        // Example: Input: 13 Output 13
+
+        var input = int.Parse(Console.ReadLine());
+
+        for (int start = input; start >= 2; start--)
+        {
+            bool isPrime = true;
+            for (int i = 2; i < Math.Sqrt(start); i++)
+            {
+                if(start % i == 0) 
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime == true) 
+            {
+                Console.WriteLine(start);
+                break;
+            }
+        }
+    }
+}
