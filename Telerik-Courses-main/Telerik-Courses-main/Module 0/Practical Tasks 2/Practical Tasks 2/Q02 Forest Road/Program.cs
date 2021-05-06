f41bf6b363895e0899632155214bb2ccbb072f5f
Uendy <alex.beshkov98@gmail.com>
2021-05-06 18:48:22 +0300

    Adding: -Q02 Forest Road (Finished)
    // I had a similar question a few months back and I couldn't solve it, now I am pretty proud, but I can also do it with a matrix instead of SB

---
+++ Telerik-Courses-main/Telerik-Courses-main/Module 0/Practical Tasks 2/Practical Tasks 2/Q02 Forest Road/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Write a map, where each step you move one down and diagonally, height = 2*N-1
+
+        var width = int.Parse(Console.ReadLine());
+        var output = new StringBuilder();
+
+        // forward
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (i == j)
+                {
+                    output.Append("*");
+                }
+                else
+                {
+                    output.Append(".");
+                }
+            }
+            output.AppendLine();
+        }
+
+        // backward
+        for (int i = width - 2; i >= 0; i--)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (i == j)
+                {
+                    output.Append("*");
+                }
+                else
+                {
+                    output.Append(".");
+                }
+            }
+            output.AppendLine();
+        }
+
+        Console.WriteLine(output.ToString());
+    }
+}
