8ef13af6aaef7d6bb4b0245f26481661479fc830
Uendy <alex.beshkov98@gmail.com>
2021-05-07 12:14:44 +0300

    Adding: -Q04 Signal From Space (Finished)

---
+++ Telerik-Courses-main/Telerik-Courses-main/Module 0/Practical Tasks 2/Practical Tasks 2/Q04 Signal From Space/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+public class Program
+{
+    public static void Main()
+    {
+        // decode a message by removing any consequently repeating characters
+        // Example: Input: hoboobbo422222Aaao Output: hobobo42Aao
+
+        var text = Console.ReadLine().ToCharArray();
+        var output = new List<char>() { text[0] };
+
+        for (int i = 1; i < text.Length; i++)
+        {
+            if (text[i] != text[i - 1])
+            {
+                output.Add(text[i]);
+            }
+        }
+
+        Console.WriteLine(string.Join("", output));
+    }
+}
