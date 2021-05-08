551ebbb5b30d2fcc097d035c5afb3a30d399b2fc
Uendy <alex.beshkov98@gmail.com>
2021-05-08 14:13:36 +0300

    Adding: -Practical Tasks 3 folder: --Q01 Array Search (Finished)

---
+++ Telerik-Courses-main/Telerik-Courses-main/Module 0/Practical Tasks 3/Practical Tasks 3/Q01 Array Search/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // given an array of numbers find all numbers from 1 to array.Count that arent elements in the array
+        // Example: Input: 1, 2, 3, 3, 5 Output: 4
+
+        var array = Console.ReadLine().Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+        var output = new List<int>();
+        for (int i = 1; i <= array.Length; i++)
+        {
+            if (!array.Contains(i))
+            {
+                output.Add(i);
+            }
+        }
+
+        Console.WriteLine(string.Join(",", output));
+    }
+}
