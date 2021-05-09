6a6e0a59238fef748d546b2e2dc9898fd6b1b577
Uendy <alex.beshkov98@gmail.com>
2021-05-09 11:02:04 +0300

    Adding: -Q03 Combine Lists (finished)

---
+++ Telerik-Courses-main/Telerik-Courses-main/Module 0/Practical Tasks 3/Practical Tasks 3/Q03 Combine Lists/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Combine two lists of the same length
+        // Example: Input: 1,2,3 4,5,6 Output: 1,4,2,5,3,6
+
+        var first = Console.ReadLine().Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        var second = Console.ReadLine().Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        var output = new List<string>();
+        for (int i = 0; i < first.Count(); i++)
+        {
+            output.Add(first[i]);
+            output.Add(second[i]);
+        }
+
+        Console.WriteLine(string.Join(",", output));
+    }
+}
