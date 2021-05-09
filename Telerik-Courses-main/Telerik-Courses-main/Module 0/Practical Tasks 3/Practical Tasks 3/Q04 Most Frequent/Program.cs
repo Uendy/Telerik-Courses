4edf5e9a347001acf3834b1843b17d826f5d3f5d
Uendy <alex.beshkov98@gmail.com>
2021-05-09 11:53:16 +0300

    Adding: -Q04 Most Frequent (Finished)
    Finished:
    -Practical Tasks 3 and Module 0

---
+++ Telerik-Courses-main/Telerik-Courses-main/Module 0/Practical Tasks 3/Practical Tasks 3/Q04 Most Frequent/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        // Find the most frequent element in array 
+        // Example: Input 1 2 3 4 3 5 5 3 Output: 3 (3 times) 
+
+        var numbers = new List<int>();
+        var inputs = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < inputs; i++)
+        {
+            numbers.Add(int.Parse(Console.ReadLine()));
+        }
+
+        var grouped = numbers.GroupBy(x => x)
+            .Select(group => new
+            {
+                num = group.Key,
+                count = group.Count()
+            })
+            .OrderByDescending(X => X.count)
+            .First();
+
+        Console.WriteLine($"{grouped.num} ({grouped.count}times)");
+    }
+}
